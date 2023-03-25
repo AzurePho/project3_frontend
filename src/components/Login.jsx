46027fd8395deb42b9d1fa75be177ffe29ab98be
Phoenix Duong <phoenixduong@hotmail.co.uk>
2023-03-25 11:14:17 +0000

    password fields updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,63 +6,60 @@ import "../Index.css";
 import "../style/Auth.css";
 
 const Login = () => {
-    const navigate = useNavigate();
-    const [error, setError] = useState("");
-    const initialFormData ={
-        email:"admin@gmail.com",
-        password: "adminPassword",
-    };
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const initialFormData = {
+    email: "admin@gmail.com",
+    password: "adminpassword",
+  };
 
-    const [formData , setFormData]= useState(initialFormData);
+  const [formData, setFormData] = useState(initialFormData);
 
-    const onChange = (e) => {
-        setError("");
-        setFormData({...formData , [e.target.name]:e.target.value});
-        console.log(formData);
+  const onChange = (e) => {
+    setError("");
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    console.log(formData);
+  };
 
-    };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+    try {
+      const { data } = await axios.post(`${API_URL}/login`, formData);
+      console.log(data.token);
+      localStorage.setItem("token", data.token);
+      axios.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
+      navigate("/drinks");
+    } catch (err) {
+      setError(err.response.data.message);
+      console.log(err);
+    }
+  };
 
-
-
-    const onSubmit = async (e) => {
-
-        e.preventDefault();
-        console.log(formData);
-        try {
-            const {data} = await axios.post(`${API_URL}/login`,formData);
-            console.log(data.token);
-            localStorage.setItem("token", data.token);
-            axios.defaults.headers.common["Authorization"]=`Bearer ${data.token}`;
-            navigate("/drinks");
-            
-        }catch(err){
-            setError(err.response.data.message);
-            console.log(err);
-        }
-};
-
-return(
+  return (
     <div className="view">
-        <h1>Login</h1>
-        {error && <h4 className="alert-failure">{error}</h4>}
+      <h1>Login</h1>
+      {error && <h4 className="alert-failure">{error}</h4>}
 
-        <form className="auth-form" onSubmit={onSubmit}>
-            <input 
-            placeholder="Email"
-            name="email"
-            value={formData.email}
-            onChange={onChange}/>
-            <input 
-            placeholder="Password"
-            name="password"
-            value={formData.password}
-            onChange={onChange} />
+      <form className="auth-form" onSubmit={onSubmit}>
+        <input
+          placeholder="Email"
+          name="email"
+          value={formData.email}
+          onChange={onChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={formData.password}
+          onChange={onChange}
+        />
 
-            <button type="submit" >Login!</button>
-        </form>
+        <button type="submit">Login!</button>
+      </form>
     </div>
-)
-
-}
+  );
+};
 
 export default Login;
